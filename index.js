88c234295e4dad38d9126c25f2638d4f0f788073
Annie <annie_baines@hotmail.co.uk>
2022-09-16 12:06:34 +0100

    add functions so +-/* calculations are performed when button is clicked and show result after Sum: text

--- index.js
+++ index.js
@@ -3,21 +3,27 @@ let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
-function add() {
+let totalSum = document.getElementById("sum-el")
 
+function add() {
+    totalSum.textContent = "Sum: "
+    totalSum.textContent += num1 + num2
 }
 
 
 function subtract() {
-
+    totalSum.textContent = "Sum: "
+    totalSum.textContent += num1 - num2
 }
 
 
 function divide() {
-
+    totalSum.textContent = "Sum: "
+    totalSum.textContent += num1 / num2
 }
 
 
 function multiply() {
-
+    totalSum.textContent = "Sum: "
+    totalSum.textContent += num1 * num2
 }
