086d5e3b42d57e7617294389774bebd1f4360249
Annie <annie_baines@hotmail.co.uk>
2022-09-16 11:48:08 +0100

    add empty maths functions to js

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ let num1 = 8
 let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+
+function add() {
+
+}
+
+
+function subtract() {
+
+}
+
+
+function divide() {
+
+}
+
+
+function multiply() {
+
+}
